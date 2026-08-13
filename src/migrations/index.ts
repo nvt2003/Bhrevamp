266fb3018c2966f9672410cb25f22d9e29028fa9
Thu <thunvhe176252@fpt.edu.vn>
2026-08-13 14:15:26 +0700

    create migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260812_091147 from './20260812_091147';
+import * as migration_20260813_071423_more_global from './20260813_071423_more_global';
 
 export const migrations = [
   {
     up: migration_20260812_091147.up,
     down: migration_20260812_091147.down,
-    name: '20260812_091147'
+    name: '20260812_091147',
+  },
+  {
+    up: migration_20260813_071423_more_global.up,
+    down: migration_20260813_071423_more_global.down,
+    name: '20260813_071423_more_global'
   },
 ];
