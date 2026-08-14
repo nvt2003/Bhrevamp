3e160c34bf7f73ff42fe62091181579f57f9122c
Thu <thunvhe176252@fpt.edu.vn>
2026-08-14 13:26:24 +0700

    remove error migration and add new

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,21 +1,9 @@
-import * as migration_20260812_091147 from './20260812_091147';
-import * as migration_20260813_071423_more_global from './20260813_071423_more_global';
-import * as migration_20260814_045748_updateAdsConfig from './20260814_045748_updateAdsConfig';
+import * as migration_20260814_062526 from './20260814_062526';
 
 export const migrations = [
   {
-    up: migration_20260812_091147.up,
-    down: migration_20260812_091147.down,
-    name: '20260812_091147',
-  },
-  {
-    up: migration_20260813_071423_more_global.up,
-    down: migration_20260813_071423_more_global.down,
-    name: '20260813_071423_more_global',
-  },
-  {
-    up: migration_20260814_045748_updateAdsConfig.up,
-    down: migration_20260814_045748_updateAdsConfig.down,
-    name: '20260814_045748_updateAdsConfig'
+    up: migration_20260814_062526.up,
+    down: migration_20260814_062526.down,
+    name: '20260814_062526'
   },
 ];
