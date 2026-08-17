24619964fa2f19a87cb4fbc928415a2143c2e935
Thu <thunvhe176252@fpt.edu.vn>
2026-08-17 11:00:26 +0700

    add image size

--- src/app/(frontend)/homepage/components/BhTvSection.tsx
+++ src/app/(frontend)/homepage/components/BhTvSection.tsx
@@ -54,6 +54,7 @@ export default function BhTvSection({ data }: { data: any }) {
                     alt={mainVideo.title}
                     fill
                     loading="lazy"
+                    sizes="(max-width: 639px) 50vw, 25vw"
                     className="w-full aspect-[16/10] object-cover group-hover:scale-105 transition-transform duration-300"
                   />
                 )}
