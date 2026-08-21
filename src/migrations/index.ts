d423d8b511e0fa52af2dae3645877a3f9f1ac0eb
Thu <thunvhe176252@fpt.edu.vn>
2026-08-21 15:37:10 +0700

    migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260819_023347 from './20260819_023347';
 import * as migration_20260819_045020 from './20260819_045020';
+import * as migration_20260821_080049 from './20260821_080049';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260819_045020.up,
     down: migration_20260819_045020.down,
-    name: '20260819_045020'
+    name: '20260819_045020',
+  },
+  {
+    up: migration_20260821_080049.up,
+    down: migration_20260821_080049.down,
+    name: '20260821_080049'
   },
 ];
